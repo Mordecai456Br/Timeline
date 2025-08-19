a7b8b0e67f9e425357883bb40b060889f4ae9d1c
Mordecai456Br <gabrielseixas1@gmail.com>
2025-08-19 20:34:40 -0300

    Create event.js

---
+++ js/event.js
@@ -0,0 +1,10 @@
+// Um evento tem uma data e uma descrição.
+
+class Event {
+    constructor(date, description){
+        this.date = date;
+        this.description = description;
+    }
+
+    
+}

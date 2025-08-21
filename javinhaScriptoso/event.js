e484c270ffc07719ecf6173d645bcdaaf8a6876f
Mordecai456Br <gabrielseixas1@gmail.com>
2025-08-20 22:31:59 -0300

    sem senha e api basica funcionando

--- javinhaScriptoso/event.js
+++ javinhaScriptoso/event.js
@@ -1,40 +1,44 @@
 // Um evento tem uma data e uma descrição.
 
 class Event {
+    #id;
     #name;
     #date;
     #description;
-    #password;
 
-    constructor(name, date, description, password){
+    constructor(id, name, date, description) {
+        this.#id = id;
         this.#name = name;
-        this.#date = date;
+        this.#date = new Date(date);
         this.#description = description;
-        this.#password = password;
+
     }
 
-    howManyTimeAgo(password){
-        if (this.#password === null || this.#password === password){
-            const currentDate = new Date;
-            const diferenceDays = Math.floor((currentDate - this.#date) / (1000 * 60 * 60 * 24));
-        
-            console.log(`${this.#name} was ${diferenceDays} days ago`)
-        }
-        else {
-            console.log("access denied: incorret password")
-        }
+    howManyTimeAgo() {
+        const currentDate = new Date;
+        const diferenceDays = Math.floor((currentDate - this.#date) / (1000 * 60 * 60 * 24));
+
+        return `${this.#name} was ${diferenceDays} days ago`;
     }
-    getInfo(password){
-        if (this.#password === null || this.#password === password){
-            return {
-                name: this.#name,
-                date: this.#date,
-                description: this.#description
-            }
-        }
-        else {
-            return "access denied: incorret password"
+
+    getInfo() {
+        return {
+            id: this.#id,
+            name: this.#name,
+            date: {
+                year: this.#date.getFullYear(),
+                month: this.#date.getMonth() + 1,
+                day: this.#date.getDate()
+            },
+            description: this.#description
         }
     }
 }
-module.exports = Event;
+//const viagemRecife = new Event("Travel to Recife",new Date(2025,7,11),"viagem para recife junto da babson");
+//const eclipse = new Event("Eclipse", new Date(2023,9,14),"eclipse na praia do jacaré c maryloponi","ola")
+const events = [
+    new Event(0, "Travel to Recife", "2025-08-11", "Viagem para recife junto da babson"),
+    new Event(1, "Eclipse", "2023-10-14", "Eclipse na praia do jacaré c maryloponi", "ola")
+];
+
+module.exports = { events, Event };
--- javinhaScriptoso/index2.js
+++ javinhaScriptoso/index2.js
@@ -1,1 +1,34 @@
-console.log("Events ahead!")
+console.log("Events ahead!");
+
+const express = require("express");
+const { events, Event } = require('./event.js');
+
+const server = express();
+server.use(express.json());
+
+// Retorna todos os eventos
+server.get("/events", (req, res) => {
+    // Mapeia as instâncias para objetos JSON
+    const eventList = events.map(e => e.getInfo());
+    res.json(eventList);
+});
+
+server.get("/events/timeAgo/:id", (req,res) => {
+    const id = parseInt(req.params.id);
+    const event = events[id];
+    res.json({message: event.howManyTimeAgo()});
+})
+
+// Cria um novo evento
+server.post("/events", (req, res) => {
+    const { name, date, description, password } = req.body;
+    const id = events.length; // ID sequencial
+    const newEvent = new Event(id, name, date, description, password);
+
+    events.push(newEvent);
+    res.json({ message: 'event added', event: newEvent.getInfo(password) });
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port: 3000");
+});

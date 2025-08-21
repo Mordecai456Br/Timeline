7d0b1e29bc16d51dda7860be9f53c9bc036b3d2a
Mordecai456Br <gabrielseixas1@gmail.com>
2025-08-21 14:17:27 -0300

    Adeus privacidade
    > Removi atributos privados, senha.
    > Adicionei Delete e Put, finalizando o restfull.
    > Implementei os retornos do http (404 e 201)
    > Arrumei as rotas

--- javinhaScriptoso/index2.js
+++ javinhaScriptoso/index2.js
@@ -6,27 +6,58 @@ const { events, Event } = require('./event.js');
 const server = express();
 server.use(express.json());
 
-// Retorna todos os eventos
+
 server.get("/events", (req, res) => {
-    // Mapeia as instâncias para objetos JSON
-    const eventList = events.map(e => e.getInfo());
-    res.json(eventList);
+    res.json( events);
 });
+
+server.get("/events/:id", (req,res) => {
+    const id = parseInt(req.params.id);
+    const event = events.find((event) => event.id === id);
+    if (!event) {
+        return res.status(404).json({message: 'event not found'})
+    }
+    res.json(event);
+})
 
 server.get("/events/timeAgo/:id", (req,res) => {
     const id = parseInt(req.params.id);
-    const event = events[id];
-    res.json({message: event.howManyTimeAgo()});
+    const event = events.find((event) => event.id === id);
+    res.json({message: event.howManyTimeAgo(), date: event.date});
 })
 
-// Cria um novo evento
+
 server.post("/events", (req, res) => {
-    const { name, date, description, password } = req.body;
-    const id = events.length; // ID sequencial
-    const newEvent = new Event(id, name, date, description, password);
+    const { name, date, description} = req.body;
+    const id = events.length; 
+    const newEvent = new Event(id, name, date, description);
 
     events.push(newEvent);
-    res.json({ message: 'event added', event: newEvent.getInfo(password) });
+    res.status(201).json({ message: 'event added', event: newEvent.getInfo() });
+});
+
+server.put("/events/:id",(req,res) => {
+    const id = Number(req.params.id);
+    const event = events.find((event) => event.id === id);
+
+    if (!event) {
+        res.status(404).json({message: 'event not found'});
+    } else {
+        Object.assign(event,req.body);
+        res.json({message: 'event updated', event});
+    }
+})
+
+server.delete("/events/:id", (req,res) => {
+    const id = Number(req.params.id);
+    const index = events.findIndex((event) => event.id === id);
+
+    if (index === -1){
+        return res.status(404).json({message: 'event not found'});
+    }
+    const deletedItem = events.splice(index,1)[0];
+    res.json({message: 'item deleted', item: deletedItem});
+
 });
 
 server.listen(3000, () => {

7ecc8d6c76a556fa2ee4d0bd33127602aa2af787
Mordecai456Br <gabrielseixas1@gmail.com>
2025-08-25 21:30:43 -0300

    Rotas arrumadas

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const eventController = require("../controllers/eventController.js")
 
 router.get('/events', eventController.listAll);
-router.get('/events',eventController.getById);
-router.get('/events',eventController.timeAgo);
+router.get('/events/:id',eventController.getById);
+router.get('/events/timeAgo/:id',eventController.timeAgo);
 router.post('/events',eventController.create);
-router.put('/events',eventController.update);
-router.delete('/events',eventController.remove);
+router.put('/events/:id',eventController.update);
+router.delete('/events/:id',eventController.remove);
 
 module.exports = router;
 
